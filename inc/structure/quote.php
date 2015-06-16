5e8f6d697181e03f4bfede34ea5ea661b0dee875
mzdon <marczdon@gmail.com>
2015-06-16 19:16:11 -0400

    Fonts introduced and quote implemented.

--- inc/structure/quote.php
+++ inc/structure/quote.php
@@ -1,5 +1,9 @@
 <?php
+	add_action( 'atg_quote', 'render_atg_quote' );
 
-
-
+	function render_atg_quote() {
+		echo '<section id="section-quote"><div class="quote-bg"></div>';
+		echo '<div class="quote-text"><h1>Great art picks up where nature ends.</h1>';
+		echo '<p class="credit">Marc Chagall</p></h3></div></section>';
+	}
 ?>
